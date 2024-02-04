0be58b5b2f666124dd31821beec5296a38aba4b9
Dariel <150453595+DarielDeveloper@users.noreply.github.com>
2024-02-04 18:43:03 -0500

    Incorporando el JavaScript

--- main.js
+++ main.js
@@ -1,135 +1,51 @@
-//Control de variables
-const tasks = [];
-let time = 0;
-let timer = null;
-let timerBreak = null;
-let current = null;
-let taskIndex = 0;
+const ratingContainer = document.querySelector('.rating');
+let currentValue = 0;
+const limit = 5;
 
-//Capturo los elementos
-const bAdd = document.querySelector('#bAdd');
-const itTask = document.querySelector('#itTask');
-const form = document.querySelector('#form');
-const taskName = document.querySelector('#time #taskName');
-const timeDiv = document.querySelector('#time #value');
+//Creo elemento de acuerdo a la cantidad 
+const html = Array.from(Array(limit)).map((_, i) => {
+  //Lo identifico por el index
+  return `<div class='item item-${i}' data-pos='${i}'></div>`;
+});
 
-renderTime();
-
-//En escucha del evento submit en el formulaio
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  if (itTask.value) {
-
-    //Creo la  tarea
-    createTask(itTask.value)
-
-    // Seteo el campo
-    itTask.value = '';
-  }
-  renderTasks();
-
-})
-
-function createTask(task) {
-  // Creo un objeto para la tarea
-  const newTask = {
-    //Creando id
-    id: (Math.random() * 100).toString(36).slice(3),
-    //Nombre de la tarea
-    title: task,
-    //Estado de la tarea
-    completed: false
-  }
-  //Incorporo la nueva tarea al principio del array
-  tasks.unshift(newTask);
-
-}
-
-function renderTasks() {
-  //Incorporo html a las tareas
-  const listTask = tasks.map(task => {
-    return `
-    <div class = 'task'>
-    <div class='completed'>
-    ${task.completed ? `<span class='done'>Completada  </span> ` : `<button class='start_button' data-id='${task.id}'>Iniciar</button>`}
-    </div>
-    <div class='title'>${task.title}</div>
-    </div>
-    `
-  });
-  //Busco el contenedor 
-  const task_coitainer = document.getElementById('tasks');
-  //Inyecto el listado al container
-  //Utilizo el join ya que es un string y de paso separo las tareas por ' '
-  task_coitainer.innerHTML = listTask.join(' ');
-
-  const buttonStart = document.querySelectorAll('.task .start_button');
-  buttonStart.forEach(button => {
-    button.addEventListener('click', e => {
-      e.preventDefault();
-      if (!timer) {
-        const id_button = button.getAttribute('data-id');
-        startButtonHandle(id_button);
-        button.textContent = 'En progreso ...';
+//Lo incorporo al HTML
+ratingContainer.innerHTML = html.join('');
+//Capturo los item
+document.querySelectorAll('.item').forEach((item) => {
+  //Activo el evento 
+  item.addEventListener('mouseover', e => {
+    //Capturo el id del elemento
+    const pos = item.getAttribute('data-pos');
+    //Validar para que no renderice el DOM en esa posición 
+    if (currentValue === parseInt(pos) + 1) {
+      return;
+    }
+    //Quito la clase 
+    document.querySelectorAll('.item').forEach((it) => {
+      if (it.classList.contains('item-full')) {
+        it.classList.remove('item-full');
       }
-    })
+    });
 
-  })
 
-}
+    //Incorporo la clase
+    for (let i = 0; i <= pos; i++) {
+      //Capturo 
+      const square = document.querySelector(`.item-${i}`);
+      if (!square.classList.contains('item-full')) {
+        square.classList.add('item-full');
+      }
 
-function startButtonHandle(id) {
-  //Control de los 25mn
-  time = 3;
-  current = id;
-  //Busco el index del elemento
-  taskIndex = tasks.findIndex((task) => task.id === id);
-  //Le incorporo el nombre de la tarea
-  taskName.textContent = tasks[taskIndex].title;
-  //Incorporo setInerval para cada 1s llame timeHandler
-  timer = setInterval(() => {
-    timeHandler();
-  }, 1000)
+    }
+    //Actualizando la posición donde se quedo
+    currentValue = parseInt(pos) + 1;
+  });
 
-}
+  //Evento click para saber donde se marca 
+  item.addEventListener('click', e => {
+    const posClick = item.getAttribute('data-pos');
+    //Posición donde se dio el click
+    currentValue = parseInt(posClick) + 1;
 
-function timeHandler() {
-  //Quito tiempo
-  time--;
-  //Compruebo hasta que llegue 0 el tiempo para detener el setInterval
-  if (time === 0) {
-
-    completedTask();
-
-  }
-  //Control del tiempo
-  renderTime();
-}
-
-function renderTime() {
-  //Control de los minutos
-  const minute = parseInt(time / 60);
-  //Control de los segundos
-  const seconds = parseInt(time % 60);
-  //Inyecto al elemnto div la cadena del time
-  //Valido en la numeracion
-  timeDiv.textContent = `${minute < 10 ? '0' : ''}${minute}:${seconds < 10 ? '0' : ''}${seconds}`
-}
-
-function completedTask() {
-  //Desactivo el interval
-  clearInterval(timer);
-  //Seteo el timer
-  timer = null;
-  //Seteo el id
-  current = null;
-  //Seteo el nombre 
-  taskName.textContent = '';
-  //Se completo la tarea
-  tasks[taskIndex].completed = true;
-  //Renderizo las tareas
-  renderTasks();
-  //Seteo el elemnto donde se muestra el time
-  timeDiv.textContent = '';
-}
+  });
+});
